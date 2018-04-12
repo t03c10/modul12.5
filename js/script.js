389802d47de7dd2cfe3c834a1d2c724b06bb8ac9
pawelz <t03c10@gmail.com>
2018-04-12 16:02:04 +0200

    add button deactivate func. and css rules

--- js/script.js
+++ js/script.js
@@ -1,20 +1,29 @@
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 var prefix = "https://cors-anywhere.herokuapp.com/";
+var button = $('.trigger');
+
 function getQuote() {
+    button.prop('disabled', true);
+    button.html('<i class="fas fa-spinner fa-spin"></i>');
+
     $.getJSON(prefix + quoteUrl, createTweet);
     $.ajaxSetup({ cache: false });
 }
 
 function createTweet(input) {
-    var data = input[0];
 
+    button.prop('disabled', false);
+    button.html('Random quote');
+
+    var data = input[0];
     var quoteText = $(data.content).text().trim();
     var quoteAuthor = data.title;
 
     if (!quoteAuthor.length) {
         quoteAuthor = "Unknown author";
     }
+    
     var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
     if (tweetText.length > 140) {
         getQuote();
